72adc20f3f4125128422453d831d8ec61c499a74
mikahuang <mikahuang@163.com>
2020-07-27 00:19:39 +0800

    Add files via upload

---
+++ webpack.development.config.js
@@ -0,0 +1,40 @@
+﻿const path = require('path');
+const Webpack = require('webpack');
+
+module.exports = {
+  entry: './src/mikavue.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'mikavue.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+        contentBase:path.resolve(__dirname,''),
+        host:'localhost',
+        port:8080,
+		stats: 'errors-only',
+        overlay: true,
+        open:true,
+        hot:true	
+  }, 
+  performance: {
+    hints: false
+  },
+  devtool: '#cheap-module-eval-source-map'
+}
---
+++ webpack.production.config.js
@@ -0,0 +1,41 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/mikavue.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'mikavue.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  optimization:{
+	  minimize: true
+  },
+  plugins:[
+  new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ],
+  devtool: '#source-map'
+}
